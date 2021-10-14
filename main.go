9643916fcbe92c550d559bc6c136d35e041e6a11
JMVoid <hook.methods@gmail.com>
2021-10-15 00:25:22 +0800

    change version command app name to Clashusk

--- main.go
+++ main.go
@@ -45,7 +45,7 @@ func init() {
 
 func main() {
 	if version {
-		fmt.Printf("Clash %s %s %s with %s %s\n", C.Version, runtime.GOOS, runtime.GOARCH, runtime.Version(), C.BuildTime)
+		fmt.Printf("Clashusk %s %s %s with %s %s\n", C.Version, runtime.GOOS, runtime.GOARCH, runtime.Version(), C.BuildTime)
 		return
 	}
 
