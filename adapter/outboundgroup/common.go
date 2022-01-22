8d230002371d66fae771a188b61f967a1c34c5c7
JMVoid <hook.methods@gmail.com>
2022-01-23 02:31:43 +0800

    fix useFilter wrong apply filter criteria to proxies.

--- adapter/outboundgroup/common.go
+++ adapter/outboundgroup/common.go
@@ -4,24 +4,33 @@ import (
 	"time"
 
 	C "github.com/Dreamacro/clash/constant"
-	"github.com/Dreamacro/clash/constant/provider"
+	types "github.com/Dreamacro/clash/constant/provider"
 )
 
 const (
 	defaultGetProxiesDuration = time.Second * 5
 )
 
-func getProvidersProxies(providers []provider.ProxyProvider, touch bool, useFilter *Filter) []C.Proxy {
-	proxies := []C.Proxy{}
+func getProvidersProxies(providers []types.ProxyProvider, touch bool, useFilter *Filter) []C.Proxy {
+	var comProxies []C.Proxy
+	var setProxies []C.Proxy
 	for _, provider := range providers {
 		if touch {
-			proxies = append(proxies, provider.ProxiesWithTouch()...)
+			if provider.VehicleType() == types.Compatible {
+				comProxies = append(comProxies, provider.ProxiesWithTouch()...)
+			} else {
+				setProxies = append(setProxies, provider.ProxiesWithTouch()...)
+			}
 		} else {
-			proxies = append(proxies, provider.Proxies()...)
+			if provider.VehicleType() == types.Compatible {
+				comProxies = append(comProxies, provider.Proxies()...)
+			} else {
+				setProxies = append(setProxies, provider.Proxies()...)
+			}
 		}
 	}
 	if useFilter.IsFilterSet {
-		return useFilter.FilterProxiesByName(proxies)
+		return append(comProxies, useFilter.FilterProxiesByName(setProxies)...)
 	}
-	return proxies
+	return append(comProxies, setProxies...)
 }
